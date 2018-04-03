c2ab87d9b719f442628a47eee5ebe8869ba1e2c6
Chris Allen <ccchris.allen@gmail.com>
2018-04-02 17:45:33 -0700

    Updating webpack config to handle dev vs prod.

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,28 @@ var path = require('path');
 var webpack = require('webpack');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-var mode = process.env.NODE_ENV ? process.env.NODE_ENV : "NONE";
+var mode = process.env.NODE_ENV ? process.env.NODE_ENV.toUpperCase() : "NONE";
+var IS_PROD = mode === 'PRODUCTION'; 
 
 console.log("--------------------------------------------------");
 console.log(`Building in ${mode.toUpperCase()} mode.`);
 console.log("--------------------------------------------------");
+
+var optional_rules = [];
+
+// transpiling to ES6 is time-consuming, so only do so for production
+if (IS_PROD) {
+    optional_rules = [
+        {
+            test: /\.js$/,
+            loaders: 'babel-loader',
+            query: {
+                presets: ['es2015']
+            }
+        }
+    ];
+}
+
 
 var config = {
     entry: './src/index.js',
@@ -18,20 +35,13 @@ var config = {
         // Note: Use 'rules' rather than 'loaders' (which is deprecated)
         rules: [
             {
-                test: /\.js$/,
-                loaders: 'babel-loader',
-                query: {
-                    presets: ['es2015']
-                }
-            },
-            {
                 test: /\.(s*)css$/,
                 use: ExtractTextPlugin.extract({
                     fallback: 'style-loader',
                     use: ['css-loader', 'sass-loader']
                 })
             }
-        ]
+        ].concat(optional_rules)
     },
     plugins: [
         new ExtractTextPlugin({filename: 'dist/style.css'})
